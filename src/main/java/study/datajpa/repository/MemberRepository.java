9eda785eed376e90c13582b36d37a8172f7ecd7d
soohoonlee <jjinimania@gmail.com>
2020-03-30 12:23:10 +0900

    벌크성 수정 쿼리

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import study.datajpa.dto.MemberDto;
@@ -45,4 +46,8 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
   //     countQuery = "select count(m) from Member m")
   @Query(value = "select m from Member m")
   Page<Member> findByAge(int age, Pageable pageable);
+
+  @Modifying(clearAutomatically = true)
+  @Query("update Member m set m.age = m.age + 1 where m.age >= :age")
+  int bulkAgePlus(@Param("age") int age);
 }
--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -5,6 +5,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -28,6 +30,9 @@ class MemberRepositoryTest {
 
   @Autowired
   TeamRepository teamRepository;
+
+  @PersistenceContext
+  EntityManager em;
 
   @Test
   void testMember() {
@@ -204,5 +209,26 @@ class MemberRepositoryTest {
     assertThat(page.getTotalPages()).isEqualTo(2);
     assertThat(page.isFirst()).isTrue();
     assertThat(page.hasNext()).isTrue();
+  }
+
+  @Test
+  void bulkUpdate() {
+    // given
+    memberRepository.save(new Member("member1", 10));
+    memberRepository.save(new Member("member2", 19));
+    memberRepository.save(new Member("member3", 20));
+    memberRepository.save(new Member("member4", 21));
+    memberRepository.save(new Member("member5", 40));
+
+    // when
+    int resultCount = memberRepository.bulkAgePlus(20);
+    // em.clear();
+
+    List<Member> result = memberRepository.findByUsername("member5");
+    Member member5 = result.get(0);
+    System.out.println("member5 = " + member5);
+
+    // then
+    assertThat(resultCount).isEqualTo(3);
   }
 }
