97933bf4c8b86b8d283bfbced7616c4a773dcdf5
soohoonlee <jjinimania@gmail.com>
2020-04-02 11:31:21 +0900

    Projections

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -347,4 +347,28 @@ class MemberRepositoryTest {
 
     assertThat(result.get(0).getUsername()).isEqualTo("m1");
   }
+
+  @Test
+  void projections() {
+    // given
+    Team teamA = new Team("teamA");
+    em.persist(teamA);
+
+    Member m1 = new Member("m1", 0, teamA);
+    Member m2 = new Member("m2", 0, teamA);
+    em.persist(m1);
+    em.persist(m2);
+
+    em.flush();
+    em.clear();
+
+    // when
+    // List<UsernameOnlyDto> result = memberRepository.findProjectionsByUsername("m1", UsernameOnlyDto.class);
+    List<NestedClosedProjections> result = memberRepository.findProjectionsByUsername("m1", NestedClosedProjections.class);
+
+    for (NestedClosedProjections usernameOnly : result) {
+      System.out.println("UserName = " + usernameOnly.getUsername());
+      System.out.println("TeamName = " + usernameOnly.getTeam().getName());
+    }
+  }
 }
