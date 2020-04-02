95743ecdda2b25000145da995dd072222719032e
soohoonlee <jjinimania@gmail.com>
2020-04-02 12:01:34 +0900

    네이티브 쿼리

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -371,4 +371,46 @@ class MemberRepositoryTest {
       System.out.println("TeamName = " + usernameOnly.getTeam().getName());
     }
   }
+
+  @Test
+  void nativeQuery() {
+    // given
+    Team teamA = new Team("teamA");
+    em.persist(teamA);
+
+    Member m1 = new Member("m1", 0, teamA);
+    Member m2 = new Member("m2", 0, teamA);
+    em.persist(m1);
+    em.persist(m2);
+
+    em.flush();
+    em.clear();
+
+    // when
+    Member result = memberRepository.findByNativeQuery("m1");
+    System.out.println("result = " + result);
+  }
+
+  @Test
+  void nativeProjectionQuery() {
+    // given
+    Team teamA = new Team("teamA");
+    em.persist(teamA);
+
+    Member m1 = new Member("m1", 0, teamA);
+    Member m2 = new Member("m2", 0, teamA);
+    em.persist(m1);
+    em.persist(m2);
+
+    em.flush();
+    em.clear();
+
+    // when
+    Page<MemberProjection> result = memberRepository.findByNativeProjection(PageRequest.of(0, 10));
+    List<MemberProjection> content = result.getContent();
+    for (MemberProjection memberProjection : content) {
+      System.out.println("memberProjection.getUsername() = " + memberProjection.getUsername());
+      System.out.println("memberProjection.getTeamname() = " + memberProjection.getTeamName());
+    }
+  }
 }
